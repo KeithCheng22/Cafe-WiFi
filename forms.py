c4c4f72b3029ec048b668e3cd45c886594485ea5
KeithCheng22 <keithcheng.sgs@gmail.com>
2023-11-10 21:08:32 +0800

    remove validator

--- forms.py
+++ forms.py
@@ -1,15 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, PasswordField, EmailField
-from wtforms.validators import DataRequired, URL, Email
+from wtforms.validators import DataRequired, URL
 
 
 class LoginForm(FlaskForm):
-    email = EmailField(label='', render_kw={"placeholder": "Email"}, validators=[DataRequired(), Email()])
+    email = EmailField(label='', render_kw={"placeholder": "Email"}, validators=[DataRequired()])
     password = PasswordField(label='', render_kw={"placeholder": "Password"}, validators=[DataRequired()])
     submit = SubmitField("Let Me In!")
 
 class RegisterForm(FlaskForm):
     name = StringField(label='', render_kw={"placeholder": "Name"}, validators=[DataRequired()])
-    email = EmailField( label='', render_kw={"placeholder": "Email"},validators=[DataRequired(), Email()])
+    email = EmailField( label='', render_kw={"placeholder": "Email"},validators=[DataRequired()])
     password = PasswordField(label='', render_kw={"placeholder": "Password"},validators=[DataRequired()])
     submit = SubmitField("Sign Me Up!")
